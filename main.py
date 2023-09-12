89298ac8e43564ea93935423eb8125c505fc6fdf
zwq <941454643@qq.com>
2023-09-12 09:04:36 +0800

    2023/9/12

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-import hashlib
-import os
-import tarfile
-import zipfile
-import requests
-# a = 1
-# assert a == 2, print("a不等于2")
-# print("end")
-# cache_dir = os.path.join('..', 'data')
-# print(cache_dir)
-def zwq(folder=None):
-    data_dir = '/data/app'
-    return os.path.join('.', folder) if folder else data_dir
-print(zwq('new_app'))
+import jieba
+text = "结巴分词是一款开源的中文分词工具。"
+words = jieba.cut(text)
+
+# 将分词结果转换为列表
+word_list = list(words)
+print(word_list[0][-1])

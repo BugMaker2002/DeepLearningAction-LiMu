4cbe3162220495d235492eae3215696723c9441f
zwq <941454643@qq.com>
2023-11-06 10:56:33 +0800

    2023/11/6

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
-import torch
-from swin_transformer import SwinTransformer
+import os
+# fname = './data/kaggle_cifar10_tiny'
+# print(os.path.dirname(fname))
+# print(os.path.splitext(fname))
+# print(os.listdir(fname))
 
-# 创建模型实例
-model = SwinTransformer(
-    img_size=(224, 224),  # 图像大小
-    patch_size=4,         # 图像分块大小
-    in_chans=3,           # 输入通道数
-    num_classes=1000,     # 类别数（根据您的任务调整）
-    embed_dim=96,         # 嵌入维度
-    depths=[2, 2, 6, 2],  # 不同阶段的Transformer块深度
-    num_heads=[3, 6, 12, 24],  # 不同阶段的注意头数
-)
+dicts = {'0': 'lgt', '1': 'zwq'}
+print(dicts.get('2', 0))
 
-# 加载预训练权重
-pretrained_weights = torch.load("path_to_pretrained_weights.pth")
-model.load_state_dict(pretrained_weights)
-import d2l
+
